f35b6d2ce56d4dae7426cf241961c75db015c743
attcrwon <ittatting@gmail.com>
2022-10-15 16:42:46 +0700

    logintest2

--- applogin.js
+++ applogin.js
@@ -1,34 +1,25 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
-import { getFirestore ,collection ,getDocs ,addDoc ,deleteDoc ,doc} from "https://www.gstatic.com/firebasejs/9.11.0/firebase-firestore.js";
-const firebaseConfig = {
-  apiKey: "AIzaSyC9Wj5xc54F8z4NcK-7RBJBuVfMh6zh-xc",
-  authDomain: "projectbarber64-9435e.firebaseapp.com",
-  projectId: "projectbarber64-9435e",
-  storageBucket: "projectbarber64-9435e.appspot.com",
-  messagingSenderId: "31761681328",
-  appId: "1:31761681328:web:9cacd2e8ed95f3b7a4a475",
-  measurementId: "G-E9B3F9EB0E"
-};
-
-const app = initializeApp(firebaseConfig);
-const database = getFirestore(app)
-const Userid = document.getElementById("Userid")
-const Passid = document.getElementById("Passid")
-
-//------------ Link Firebase เข้ากับแอพ------------
-async function getEmployees(database){
-  const empCol = collection(database,'employees')
-  const empDocument =getDocs(empCol)
-  return empDocument
-}
-
 //ตรวจสอบรหัส
-function myFunction(){
-    if(Userid == "admin" || Passid == "admin"){
-      var x = 10;
-      document.getElementById("x").innerHTML = x;
-    }
+const Userid = document.getElementById('Userid')
+const Passid = document.getElementById('Passid')
+function login(){
+  if(Userid == "admin" || Passid == "admin"){
+    
+  }else{
+    alert ("เข้าสู่ระบบไม่สำเร็จ")
+  }
+  return login;
 }
+
+function go(){
+  window.open("indexAdmin.html")
+}
+
+function go2(){
+  window.open("indexBarber.html")
+}
+
+    
+
 
 
   
