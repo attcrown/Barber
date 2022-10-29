e2e70b40c2460edf6b7d45f67ce79e320655e790
attcrwon <ittatting@gmail.com>
2022-10-29 15:01:17 +0700

    Edit windows loads

---
+++ Nav.js
@@ -0,0 +1,43 @@
+let userlink = document.getElementById("userlink");
+let signoutlink = document.getElementById('signoutlink');
+var currentuser = null;
+
+//-------windows loads--------
+window.onload = function(){
+    getUsername();
+    if(currentuser == null){
+        window.location = "login.html";
+    }
+    else if(currentuser.rank == "barber"){
+        window.location = "indexBarber.html";
+    }
+    else{
+        userlink.innerText = "User : "+currentuser.username;
+        // header.innerText = "welcome " + currentuser.fullname;
+        userlink.classList.replace("nav-link","btn");
+        userlink.classList.remove("btn-primary");
+        userlink.href = "#";
+    }
+}
+
+signoutlink.addEventListener('click',Signouts);
+
+    function Signouts(){
+        sessionStorage.removeItem("user");
+        localStorage.removeItem("user");
+        localStorage.removeItem("keepLoggedIn");
+        window.location = "login.html";
+    }
+
+//----function----
+function getUsername(){
+    let keepLoggedIn = localStorage.getItem("keepLoggedIn");
+    if(keepLoggedIn == "yes"){
+        currentuser = JSON.parse(localStorage.getItem("user"));
+    }
+    else{
+        currentuser = JSON.parse(sessionStorage.getItem("user"));
+    }
+    //console.log(keepLoggedIn);
+    //console.log(currentuser);
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# web-platform-uhsngb
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-uhsngb)
