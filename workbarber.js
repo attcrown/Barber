04b99af17b7acc636c1e1ec676411b031dca3a60
attcrwon <ittatting@gmail.com>
2023-02-11 13:12:28 +0700

    EditDatebarber

--- workbarber.js
+++ workbarber.js
@@ -28,6 +28,18 @@ if(datenum < 10){
 let year = datenow.getFullYear();
 let datesub = year+"-"+month+"-"+datenum;
 document.getElementById('datenow').innerHTML = "Date "+datesub;
+const arrayTimeshow = ["00:00","00:30","01:00","01:30","02:00","02:30","03:00"
+                  ,"03:30","04:00","04:30","05:00","05:30","06:00","06:30"
+                  ,"07:00","07:30","08:00","08:30","09:00","09:30","10:00"
+                  ,"10:30","11:00","11:30","12:00","12:30","13:00","13:30"
+                  ,"14:00","14:30","15:00","15:30","16:00","16:30","17:00"
+                  ,"17:30","18:00","18:30","19:00","19:30","20:00","20:30"
+                  ,"21:00","21:30","22:00","22:30","23:00","23:30"];
+const arrayTimeDayshow = ['01','02','03','04','05','06','07','08','09','10'
+                          ,'11','12','13','14','15','16','17','18','19','20'
+                          ,'21','22','23','24','25','26','27','28','29','30','31'];                  
+const arrayTimeMonthshow = ['01','02','03','04','05','06','07','08','09'
+                            ,'10','11','12'];
 //--------เก็บค่าจาก id------
 const dbRefuser = ref(db,'userLineliff/'); 
 const dbReff = ref(db);
@@ -35,29 +47,43 @@ const dbReff = ref(db);
 
 //----------show-------
 showdata();
-function showdata(){
-  $('#table td').remove();
-  var rowNum = 0; 
-  onValue(dbRefuser,(snapshot)=>{ //--อ่านฐานข้อมูล----
+async function showdata(){
+  document.getElementById("timeqq").innerText = "เวลา";
+  document.getElementById("nameuserqq").innerText = "ลูกค้า";
+  document.getElementById("namebarberqq").innerText = "ช่าง";
+  $('#table td').remove(); 
+  var rowNum = 0;
+  let check = [];
+  for(let i = 0; i<arrayTimeshow.length; i++){
+    check[i] = [];
+  }
+  var row;
+  await onValue(dbRefuser,(snapshot)=>{ //--อ่านฐานข้อมูล----
     snapshot.forEach((childSnapshot)=>{
       const childKey = childSnapshot.key;
       const childData = childSnapshot.val();
-      // if(childData.summinute != ""){
+      var iduser = childKey; 
       if(childData.perple == pullnames && childData.summinute != "" 
-      && childData.date == datesub){
-        rowNum += 1;       
-        var iduser = childKey;        
-        var row = "<tr><td>" + rowNum + "</td><td>" + childData.time + 
-        "</td><td>" + childData.name + "</td>" //<td>"+ childData.perple + 
-        +"<td><button class='btn btn-success' data-bs-toggle='modal' data-bs-target='#exampleModal' id='"+childData.name+"' value="
-        + iduser + " style='--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;' onclick='CancelQ(id,value)'>ข้อมูล</button></td></tr>";
-      }
-      $(row).appendTo('#table');             
-      });
-
-      //--------Function Popup ปุ่มลบ
-      document.getElementById('ConfirmDel').addEventListener('click',(e)=>{
-        get(child(dbReff,"userLineliff/"+ deluserid)).then(function(snapshot){
+      && childData.date == datesub && childData != ""){
+        for(let i = 0;i<arrayTimeshow.length;i++){
+          if(childData.time == arrayTimeshow[i]){
+            check[i] = `<td>${childData.time} 
+            </td><td>${childData.name}</td><td>${childData.perple} 
+            </td><td><button class='btn btn-success' data-bs-toggle='modal' 
+            data-bs-target='#exampleModal' 
+            id='${childData.name}' 
+            value='${iduser}' 
+            style='--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; 
+            --bs-btn-font-size: .75rem;'
+            onclick='CancelQ(id,value)'>ข้อมูล</button></td></tr>`;
+          }
+          //console.log(check);
+        }
+      }    
+    });
+    //--------Function Popup ปุ่มลบ
+    document.getElementById('ConfirmDel').addEventListener('click',(e)=>{
+        get(child(dbRef,"userLineliff/"+ deluserid)).then(function(snapshot){
           if(snapshot.exists){
             update(ref(db,"userLineliff/"+ deluserid),{
               summinute : ""
@@ -69,13 +95,287 @@ function showdata(){
             });
           } else{alert("ไม่เจอข้อมูล UserID");}
         })
-      });     
+      });              
+  },{
+    onlyOnce: true
+  });  
+  setTimeout(()=>{
+    numberQQ();
+  },500);
+  function numberQQ(){
+    for(let k =0;k<check.length;k++){
+      if(check[k] != null && check[k] != ""){
+      rowNum++;
+      row = `<tr><td>${rowNum}</td>`+check[k];
+      $(row).appendTo('#table');}
+    }    
+  }
+}  
+
+const Showdatework = document.getElementById('Showdatework');
+Showdatework.addEventListener('change',(e)=>{
+  //========day=========
+  if(Showdatework.value == 'Sday'){
+    showdata();
+  }
+  //=======Week===========
+  if(Showdatework.value == "Sweeks"){
+    document.getElementById("timeqq").innerText = "วันที่";
+    document.getElementById("nameuserqq").innerText = "เวลา";
+    document.getElementById("namebarberqq").innerText = "ลูกค้า";
+    $('#table td').remove();
+    var rowNum = 0;
+    let check = [];
+    for(let i = 0; i<arrayTimeDayshow.length; i++){
+      check[i] = [];
+      for(let o = 0; o<arrayTimeshow.length; o++){
+        check[i][o] = [];
+      }
+    }
+    var row;
+    let now;
+    let week; 
+    now= Number.parseInt(datesub.substring(8,10));
+    week = Number.parseInt(datesub.substring(8,10))+7;
+    onValue(dbRefuser,(snapshot)=>{ //--อ่านฐานข้อมูล----
+      snapshot.forEach((childSnapshot)=>{
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        var iduser = childKey; 
+        if(childData.perple == pullnames && childData.summinute != "" 
+        && childData != "" 
+        && childData.date.substring(5,7) == datesub.substring(5,7) //เทียบเดือน
+        && Number.parseInt(childData.date.substring(8,10)) >= now  //เทียบวัน
+        && Number.parseInt(childData.date.substring(8,10)) <= week){
+          for(let i = 0; i<arrayTimeDayshow.length; i++){
+            if(childData.date.substring(8,10) == arrayTimeDayshow[i]){
+              for(let o = 0; o<arrayTimeshow.length; o++){
+                if(childData.time == arrayTimeshow[o]){             
+                  check[i][o].push(`<td>${childData.date} 
+                  </td><td>${childData.time}</td><td>${childData.name} 
+                  </td><td><button class='btn btn-success' data-bs-toggle='modal' 
+                  data-bs-target='#exampleModal' 
+                  id='${childData.name}' 
+                  value='${iduser}' 
+                  style='--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; 
+                  --bs-btn-font-size: .75rem;'
+                  onclick='CancelQ(id,value)'>ข้อมูล</button></td></tr>`);                  
+                }else{}                                          
+              }                
+            }   
+          }
+        }
+      });
+      //--------Function Popup ข้อมูล------
+      document.getElementById('ConfirmDel').addEventListener('click',(e)=>{
+          get(child(dbRef,"userLineliff/"+ deluserid)).then(function(snapshot){
+            if(snapshot.exists){
+              update(ref(db,"userLineliff/"+ deluserid),{
+                summinute : ""
+              }).then(function(){
+                setTimeout(() => {
+                  document.location.reload();
+                }, 500);
+                return;
+              });
+            } else{alert("ไม่เจอข้อมูล UserID");}
+          })
+        });              
+    }, 
+    {
+      onlyOnce: true
+    });
+    setTimeout(()=>{
+      numberQQ();
+    },300);
+    function numberQQ(){
+      for(let k =0; k<check.length; k++){
+        if(check[k] != null && check[k] != ""){
+          for(let p = 0; p <check[k].length; p++){
+            if(check[k][p] != null && check[k][p] != ""){
+              rowNum++;
+              row = `<tr><td>${rowNum}</td>`+check[k][p];
+              $(row).appendTo('#table');
+            }
+          }
+        }        
+      }      
+    }
+  }
+  //======Month=========
+  if(Showdatework.value == "Smonths"){
+    document.getElementById("timeqq").innerText = "วันที่";
+    document.getElementById("nameuserqq").innerText = "เวลา";
+    document.getElementById("namebarberqq").innerText = "ลูกค้า";
+    $('#table td').remove();
+    let check = [];
+    for(let d = 0; d<arrayTimeDayshow.length; d++){
+      check[d] = [];
+      for(let t = 0;t<arrayTimeshow.length;t++){
+        check[d][t] = [];
+      }
+    }
+    var rowNum = 0; 
+    var row;
+    let now;
+    let week; 
+    now= 1;
+    week = 31;
+    onValue(dbRefuser,(snapshot)=>{ //--อ่านฐานข้อมูล----
+      snapshot.forEach((childSnapshot)=>{
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        var iduser = childKey; 
+        if(childData.perple == pullnames && childData.summinute != "" 
+        && childData != "" 
+        && childData.date.substring(5,7) == datesub.substring(5,7) 
+        && Number.parseInt(childData.date.substring(8,10)) >= now 
+        && Number.parseInt(childData.date.substring(8,10)) <= week){
+          for(let i = 0; i<arrayTimeDayshow.length; i++){
+            if(childData.date.substring(8,10) == arrayTimeDayshow[i]){
+              for(let o = 0; o<arrayTimeshow.length; o++){
+                if(childData.time == arrayTimeshow[o]){             
+                  check[i][o].push(`<td>${childData.date} 
+                  </td><td>${childData.time}</td><td>${childData.name} 
+                  </td><td><button class='btn btn-success' data-bs-toggle='modal' 
+                  data-bs-target='#exampleModal' 
+                  id='${childData.name}' 
+                  value='${iduser}' 
+                  style='--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; 
+                  --bs-btn-font-size: .75rem;'
+                  onclick='CancelQ(id,value)'>ข้อมูล</button></td></tr>`);                  
+                }else{}                                          
+              }                
+            }   
+          }
+        }
+      });
+      //--------Function Popup ข้อมูล
+      document.getElementById('ConfirmDel').addEventListener('click',(e)=>{
+          get(child(dbRef,"userLineliff/"+ deluserid)).then(function(snapshot){
+            if(snapshot.exists){
+              update(ref(db,"userLineliff/"+ deluserid),{
+                summinute : ""
+              }).then(function(){
+                setTimeout(() => {
+                  document.location.reload();
+                }, 500);
+                return;
+              });
+            } else{alert("ไม่เจอข้อมูล UserID");}
+          })
+        });              
+    }, 
+    {
+      onlyOnce: true
+    });
+    setTimeout(()=>{
+      numberQQ();
+    },300);
+    function numberQQ(){
+      for(let k =0; k<check.length; k++){
+        if(check[k] != null && check[k] != ""){
+          for(let p = 0; p <check[k].length; p++){
+            if(check[k][p] != null && check[k][p] != ""){
+              rowNum++;
+              row = `<tr><td>${rowNum}</td>`+check[k][p];
+              $(row).appendTo('#table');
+            }
+          }
+        }        
+      }      
+    }
+  }
+  //======ALL==========
+  if(Showdatework.value == "Sall"){
+    document.getElementById("timeqq").innerText = "วันที่";
+    document.getElementById("nameuserqq").innerText = "เวลา";
+    document.getElementById("namebarberqq").innerText = "ลูกค้า";
+    $('#table td').remove();
+    $('#table td').remove();
+    let check = [];
+    for(let m = 0;m<arrayTimeMonthshow.length; m++){
+      check[m] = [];
+      for(let d = 0; d<arrayTimeDayshow.length; d++){
+        check[m][d] = [];
+        for(let t = 0;t<arrayTimeshow.length;t++){
+          check[m][d][t] = [];
+        }
+      }
+    }
+    var row;
+    var rowNum = 0;  
+    onValue(dbRefuser,(snapshot)=>{ //--อ่านฐานข้อมูล----
+      snapshot.forEach((childSnapshot)=>{
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        var iduser = childKey; 
+        if(childData.perple == pullnames && childData.summinute != "" 
+        && childData != ""){
+          for(let m = 0; m<arrayTimeMonthshow.length; m++){
+            if(childData.date.substring(5,7) == arrayTimeMonthshow[m]){
+              for(let i = 0; i<arrayTimeDayshow.length; i++){
+                if(childData.date.substring(8,10) == arrayTimeDayshow[i]){
+                  for(let o = 0; o<arrayTimeshow.length; o++){
+                    if(childData.time == arrayTimeshow[o]){             
+                      check[m][i][o].push(`<td>${childData.date} 
+                      </td><td>${childData.time}</td><td>${childData.name} 
+                      </td><td><button class='btn btn-success' data-bs-toggle='modal' 
+                      data-bs-target='#exampleModal' 
+                      id='${childData.name}' 
+                      value='${iduser}' 
+                      style='--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; 
+                      --bs-btn-font-size: .75rem;'
+                      onclick='CancelQ(id,value)'>ข้อมูล</button></td></tr>`);                  
+                    }else{}                                          
+                  }                
+                }   
+              }
+            }           
+          }
+        }
+      });
+      //--------Function Popup ข้อมูล
+      document.getElementById('ConfirmDel').addEventListener('click',(e)=>{
+        get(child(dbRef,"userLineliff/"+ deluserid)).then(function(snapshot){
+          if(snapshot.exists){
+            update(ref(db,"userLineliff/"+ deluserid),{
+              summinute : ""
+            }).then(function(){
+              setTimeout(() => {
+                document.location.reload();
+              }, 500);
+              return;
+            });
+          } else{alert("ไม่เจอข้อมูล UserID");}
+        })
+      });              
   }, 
   {
     onlyOnce: true
   });
+  setTimeout(()=>{
+    numberQQ();
+  },300);
+  function numberQQ(){
+    for(let m =0; m<check.length; m++){
+      if(check[m] != null && check[m] != ""){
+        for(let k =0; k<check[m].length; k++){
+          if(check[m][k] != null && check[m][k] != ""){
+            for(let p = 0; p <check[m][k].length; p++){
+              if(check[m][k][p] != null && check[m][k][p] != ""){
+                rowNum++;
+                row = `<tr><td>${rowNum}</td>`+check[m][k][p];
+                $(row).appendTo('#table');
+              }
+            }
+          }        
+        }  
+      }        
+    }          
+  }
 }
-
+})
 //----namebarber Value-----
 const btnaddtime = document.getElementById('btnaddtime');
 const nameuser = document.getElementById('nameuser');
@@ -274,7 +574,9 @@ document.getElementById('popuplink').addEventListener('click',(e)=>{
   get(child(dbReff,"userLineliff/"+ deluserid)).then(function(snapshot){
     const v = snapshot.val();
     console.log(v);
+    document.getElementById('textbarberpopup').innerText = `ชื่อช่าง : ${v.perple}`;
     document.getElementById('popupdate').innerText = `เวลาการจอง : ${v.time}`;
+    document.getElementById('popupdateq').innerText = `วันที่จอง : ${v.date}`;
     if(v.encodedImage != "" && v.encodedImage != undefined 
     && v.encodedImage != null && v.name == delname){
       document.getElementById('slipmoney').src = v.encodedImage;
@@ -290,13 +592,41 @@ document.getElementById('popuplink').addEventListener('click',(e)=>{
 });
 
 document.getElementById('closepopup').addEventListener('click',(e)=>{
+  document.getElementById('textbarberpopup').innerText = "";
   document.getElementById('slipmoney').src = "";
   document.getElementById('popupdate').innerText = "";
   document.getElementById('phonenumber').innerText = "";
+  document.getElementById('popupdateq').innerText = "";
 })
 
 document.getElementById('btnclosepopup').addEventListener('click',(e)=>{
+  document.getElementById('textbarberpopup').innerText = "";
   document.getElementById('slipmoney').src = "";
   document.getElementById('popupdate').innerText = "";
   document.getElementById('phonenumber').innerText = "";
+  document.getElementById('popupdateq').innerText = "";
+})
+
+document.getElementById('ConfirmDel').addEventListener('click',(e)=>{
+  document.getElementById('textbarberpopup').innerText = "";
+  document.getElementById('slipmoney').src = "";
+  document.getElementById('popupdate').innerText = "";
+  document.getElementById('phonenumber').innerText = "";
+  document.getElementById('popupdateq').innerText = "";
+})
+
+document.getElementById('closepopup2').addEventListener('click',(e)=>{
+  document.getElementById('textbarberpopup').innerText = "";
+  document.getElementById('slipmoney').src = "";
+  document.getElementById('popupdate').innerText = "";
+  document.getElementById('phonenumber').innerText = "";
+  document.getElementById('popupdateq').innerText = "";
+})
+
+document.getElementById('btnclosepopup2').addEventListener('click',(e)=>{
+  document.getElementById('textbarberpopup').innerText = "";
+  document.getElementById('slipmoney').src = "";
+  document.getElementById('popupdate').innerText = "";
+  document.getElementById('phonenumber').innerText = "";
+  document.getElementById('popupdateq').innerText = "";
 })
